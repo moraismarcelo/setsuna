cacd006dabcb10d1510cd1cf2749c7b249512abb
Marcelo Morais <morais.marcelo@outlook.com>
2020-01-10 01:52:51 -0300

    Exercícios da Aula 6-2

---
+++ python/a6e2.py
@@ -0,0 +1,7 @@
+entrada = input('Digite algum valor: ')
+print('O tipo primitivo deste valor é ', type(entrada))
+print('O valor é numérico? ', entrada.isalnum())
+print('O valor é alfabético? ', entrada.isalpha())
+print('O valor é alfanumérico? ', entrada.isnumeric())
+
+
